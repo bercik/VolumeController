77fa33af565b61dd6332429e8b105aa459b9c374
Robert <robert.cebula1@gmail.com>
2016-01-11 23:18:26 +0100

    add notification

--- android/VolumeController/app/src/main/java/pl/rcebula/volumecontroller/MainActivity.java
+++ android/VolumeController/app/src/main/java/pl/rcebula/volumecontroller/MainActivity.java
@@ -1,5 +1,8 @@
 package pl.rcebula.volumecontroller;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -9,6 +12,7 @@ import android.graphics.Color;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.StrictMode;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -178,6 +182,41 @@ public class MainActivity extends ActionBarActivity
         };
 
         registerReceiver(wifiReceiver, filter);
+
+        createNotification();
+    }
+
+    private void createNotification()
+    {
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.drawable.ic_stat_name)
+                        .setContentTitle("Volume Controller")
+                        .setContentText("")
+                        .setProgress(100, svSeekBar.getProgress(), false)
+                        .setAutoCancel(false);
+        // Creates an explicit intent for an Activity in your app
+        Intent resultIntent = new Intent(this, MainActivity.class);
+
+        // The stack builder object will contain an artificial back stack for the
+        // started Activity.
+        // This ensures that navigating backward from the Activity leads out of
+        // your application to the Home screen.
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        // Adds the back stack for the Intent (but not the Intent itself)
+        stackBuilder.addParentStack(MainActivity.class);
+        // Adds the Intent that starts the Activity to the top of the stack
+        stackBuilder.addNextIntent(resultIntent);
+        PendingIntent resultPendingIntent =
+                stackBuilder.getPendingIntent(
+                        0,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                );
+        mBuilder.setContentIntent(resultPendingIntent);
+        NotificationManager mNotificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        // 0 allows you to update the notification later on.
+        mNotificationManager.notify(0, mBuilder.build());
     }
 
     private void getVolume()
@@ -289,5 +328,8 @@ public class MainActivity extends ActionBarActivity
     {
         super.onDestroy();
         unregisterReceiver(wifiReceiver);
+        NotificationManager mNotificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        mNotificationManager.cancel(0);
     }
 }
