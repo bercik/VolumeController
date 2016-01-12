0903b4c1a149e458f7ca6b208a71e898e6406a74
Robert <robert.cebula1@gmail.com>
2016-01-12 16:24:45 +0100

    Add open and close url utility, new way of startup

--- pc/linux/VolumeController.py
+++ pc/linux/VolumeController.py
@@ -1,7 +1,9 @@
 import time
 import subprocess
 import socket
+import datetime
 import sys
+import logging
 
 class VolumeController:
     def setVolume(self, newVolume):
@@ -19,14 +21,34 @@ class VolumeController:
 
         return int(out[:-2])
 
-class Server:
+class BrowserController:
+    def __init__(self):
+        self.proc = None
 
+    def openURL(self, url):
+        if self.proc != None:
+            self.closeURL()
+
+        logging.debug('open url: ' + url)
+        self.proc = subprocess.Popen(['google-chrome',  '--user-data-dir=/home/chrome-user', url])
+        # self.proc = subprocess.Popen(['firefox',  '-new-window', url])
+
+    def closeURL(self):
+        if self.proc != None:
+            logging.debug('close url: ')
+            self.proc.terminate()
+            self.proc = None
+
+class Server:
     SET_VOLUME = "SET_VOL"
     GET_VOLUME = "GET_VOL"
+    OPEN_URL = "OPEN_URL"
+    CLOSE_URL = "CLOSE_URL"
 
     def __init__(self, port):
         self.port = port
         self.vc = VolumeController()
+        self.bc = BrowserController()
 
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.bind(('', self.port))
@@ -37,6 +59,7 @@ class Server:
         try:
             recived = clientSocket.recv(1024)
             recived = recived.decode(encoding='UTF-8')
+            recived = recived[:-1] # remove \n character from end
 
             if recived.startswith(self.GET_VOLUME):
                 vol = str(self.vc.getVolume()) + '\n'
@@ -44,20 +67,43 @@ class Server:
             elif recived.startswith(self.SET_VOLUME):
                 vol = int(recived.split(' ')[1])
                 self.vc.setVolume(vol)
+            elif recived.startswith(self.OPEN_URL):
+                url = recived.split(' ')[1]
+                self.bc.openURL(url)
+            elif recived.startswith(self.CLOSE_URL):
+                self.bc.closeURL()
+
         except Exception as ex:
-            print(ex)
+            logging.error(ex)
 
     def close(self):
         self.socket.close()
 
 if __name__ == '__main__':
+
+    LOG_FILENAME = '/home/robert/volume-controller.log'
+    FORMAT = '%(asctime)-15s %(levelname)s: %(message)s'
+    logging.basicConfig(filename=LOG_FILENAME, level=logging.DEBUG)
+
+    logging.debug('--DEBUG--')
+    logging.debug(datetime.datetime.now())
+
     server = None
 
-    while True:
-        server = Server(5656)
-
+    try:
         while True:
-            server.accept()
+            try:
+                server = Server(5656)
 
+                while True:
+                    server.accept()
+            except Exception as ex:
+                logging.error(ex)
+                if server != None:
+                    server.close()
+
+                time.sleep(1)
+    except Exception as ex:
+        logging.error(ex)
         if server != None:
             server.close()
---
+++ pc/linux/run_mate_terminal.sh
@@ -0,0 +1,1 @@
+mate-terminal --working-directory=/home/robert/programowanie/VolumeController/pc/linux --geometry=0x0+0+0 --title="Volume Controller" -e "python3 ./VolumeController.py"
